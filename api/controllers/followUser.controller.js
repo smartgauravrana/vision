5ac18754a1833542e7a71b1338e5d86520dc5031
chirag2838 <malhotrachirag2838@gmail.com>
2018-08-21 12:26:50 +0530

    follow api done

--- api/controllers/followUser.controller.js
+++ api/controllers/followUser.controller.js
@@ -5,37 +5,71 @@ module.exports.followUser = (req, res) => {
 
 	if (req.body.user1 && req.body.user2) {
 
-		User
-			.findOne({'username': req.body.user1})
-			.select('following')
-			.exec((err, followingArr) => {
+		User.find({$and: [{'username': req.body.user1}, {'following': {$elemMatch: {'username': req.body.user2}}}]}, (err1, data1) => {
 
-				if (err) {
+			if (err1) {
 
-					console.log(err);
-				}
+				console.log(err1);
+			}
 
-				else if (!followingArr) {
+			else if (data1.length === 0) {
 
-					res.json({success: false, message: 'User not found'});
-				}
+				User.findOneAndUpdate({'username': req.body.user1}, {$push: {'following': {'username': req.body.user2}}}, (err2, data2) => {
 
-				else {
+					if (err2) {
 
-					followingArr.find({'username': req.body.user2}, (err, data) => {
-						
-						if (err) {
-							console.log(err);
-						}
+						console.log(err2);
+					}
 
-						console.log('data', data);
-						res.json({data: data});
-					})
-				}
-			})
+					else {
+
+						User.findOneAndUpdate({'username': req.body.user2}, {$push: {'followers': {'username': req.body.user1}}}, (err3, data3) => {
+							
+							if (err3) {
+
+								console.log(err3);
+							}
+
+							else {
+
+								res.json({success: true, message: req.body.user1 + " started following " + req.body.user2});
+							}
+						});
+					}
+				});
+			}
+
+			else {
+				
+				User.findOneAndUpdate({'username': req.body.user1}, {$pull: {'following': {'username': req.body.user2}}}, (err4, data4) => {
+
+					if (err4) {
+
+						console.log(err4);
+					}
+
+					else {
+
+						User.findOneAndUpdate({'username': req.body.user2}, {$pull: {'followers': {'username': req.body.user1}}}, (err5, data5) => {
+
+							if (err5) {
+
+								console.log(err5);
+							}
+
+							else {
+
+								res.json({success: true, message: req.body.user1 + ' unfollowed ' + req.body.user2});
+							}
+						});
+					}
+				});
+			}
+		});
 	}
 
 	else {
+
 		res.json({success: false, message: 'username not provided'});
 	}
 }
