213b732e437ecbb7eaef32c43f8f07585f118b2a
chirag2838 <malhotrachirag2838@gmail.com>
2018-08-23 14:16:54 +0530

    toolbar completed

--- client/src/components/Navigation/Toolbar/Toolbar.js
+++ client/src/components/Navigation/Toolbar/Toolbar.js
@@ -5,30 +5,37 @@ import Button from '../../UI/Button/Button';
 import Logo from '../../UI/Logo/Logo';
 import Searchbar from '../../../containers/Searchbar/Searchbar';
 import Notification from '../../../containers/Notification/Notification';
+import Username from '../../../containers/Auth/Username/Username';
 
 const Toolbar = props => {
+
+    const buttonClasses = ["col-md-2 offset-md-1", classes.Font];
+    const logoClasses = ["col-md-1 offset-md-2", classes.LogoFont];
+    const notificationClasses = ["col-md-1", classes.TopMargin, classes.navItemColors];
+    const usernameClasses = ["col-md-1", classes.Font, classes.TopMargin, classes.navItemColors];
+    const logoutClasses = ["col-md-1", classes.Font]
 
     return (
         <Aux>
             <div className={classes.Toolbar}>
                 <div className="row">
-                    <div className="col-md-2 offset-md-1">
-                        <div className={classes.Font}>
-                            <Button>UPGRADE TO PRO</Button>
-                        </div>
+                    <div className={buttonClasses.join(' ')}>
+                        <Button>UPGRADE TO PRO</Button>
                     </div>
-                    <div className="col-md-1 offset-md-2">
-                        <div className={classes.Font}>
-                            <Logo />
-                        </div>
+                    <div className={logoClasses.join(' ')}>
+                        <Logo />
                     </div>
                     <div className="col-md-2 offset-md-1">
                         <Searchbar />
                     </div>
-                    <div className="col-md-1">
-                        <div className={classes.Notification}>
-                            <Notification />
-                        </div>
+                    <div className={notificationClasses.join(' ')}>
+                        <Notification />
+                    </div>
+                    <div className={usernameClasses.join(' ')}>
+                        <Username />
+                    </div>
+                    <div className={logoutClasses.join(' ')}>
+                        <Button>Logout</Button>
                     </div>
                 </div>
             </div>
---
+++ client/src/containers/Auth/Username/Username.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+
+class Username extends Component {
+
+    render () {
+
+        return (
+
+            <Aux>
+                Username
+            </Aux>
+        );
+    }
+}
+
+export default Username;
