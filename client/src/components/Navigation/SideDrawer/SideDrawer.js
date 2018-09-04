1e5f69cd4bf7fa7ed143a867e48c2b5430381b25
chirag2838 <malhotrachirag2838@gmail.com>
2018-09-04 10:14:21 +0530

    sidedrawer items alligned

--- client/src/components/Navigation/SideDrawer/SideDrawer.js
+++ client/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from '../../UI/Button/Button';
-import Notification from '../../../containers/Notification/Notification';
 import Logo from '../../UI/Logo/Logo';
 import classes from './SideDrawer.css';
 
 const SideDrawer = props => {
 
+    const logoClasses = ["row", classes.Logo]
     let sideDrawerClasses = ["container-fluid", classes.SideDrawer, classes.Close];
 
     if (props.open) {
@@ -15,14 +15,16 @@ const SideDrawer = props => {
 
     return (
         <div className={sideDrawerClasses.join(' ')} onClick={props.close}>
-            <div className="row">
-                <Logo />
+            <div className={logoClasses.join(' ')}>
+                <div className="col-sm-8 offset-sm-2">
+                    <Logo />
+                </div>
             </div>
             <div className="row">
                 <Button>UPGRADE TO PRO</Button>
             </div>
             <div className="row">
-                <Notification />
+                <Button>Notification</Button>
             </div>
             <div className="row">
                 <Button>My Profile</Button>
