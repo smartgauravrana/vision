8dac924387ad9f5cc4bb1f7abdf4ff8c7706f382
Gaurav Rana <smartgauravrana@gmail.com>
2018-11-13 21:28:40 +0530

    dev keys added

---
+++ api/config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'facebookAuth': {
+        'clientId': '279197546216728',
+        'clientSecret': '6c05f8543380cb1eea7367d0ef915790',
+        'callbackURL': 'http://localhost:3000/api/auth/facebook/callback'
+    },
+    'googleAuth': {
+        'clientId': '1047730463472-7au76qdssi5ganae0bpfm9org4f4uo69.apps.googleusercontent.com',
+        'clientSecret': 'hV1prtJ0QmjBjFp4d3-Qd8jd',
+        'callbackURL': 'http://localhost:3000/api/auth/google/callback'
+    }
+}
---
+++ api/config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'facebookAuth': {
+        'clientId': process.env.FACEBOOK_CLIENT_ID,
+        'clientSecret': process.env.FACEBOOK_CLIENT_SECRET,
+        'callbackURL': 'http://localhost:3000/api/auth/facebook/callback'
+    },
+    'googleAuth': {
+        'clientId': process.env.GOOGLE_CLIENT_ID,
+        'clientSecret': process.env.GOOGLE_CLIENT_SECRET,
+        'callbackURL': 'http://localhost:3000/api/auth/google/callback'
+    }
+}
