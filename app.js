ec96b22f0185b461eba96a4766de3dcad4533c6b
chirag2838 <malhotrachirag2838@gmail.com>
2018-11-19 12:47:03 +0530

    react build gen

--- app.js
+++ app.js
@@ -30,11 +30,24 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.use('/api', routes);
 
